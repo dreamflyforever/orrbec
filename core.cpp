be673470ec266484c0b15434518a8272d415e3e9
jimmy <jimmy.yang@moprobo.com>
2023-09-25 17:52:21 +0800

    fix no picture
    Signed-off-by: jimmy <jimmy.yang@moprobo.com>

--- core.cpp
+++ core.cpp
@@ -45,115 +45,10 @@ void saveColor(std::shared_ptr<ob::ColorFrame> colorFrame) {
     std::cout << "Color saved:" << colorName << std::endl;
 }
 
-#if 0
-int main(int argc, char **argv) try {
-    // 创建pipeline
-    ob::Pipeline pipeline;
-    // 通过创建Config来配置Pipeline要启用或者禁用哪些流
-    std::shared_ptr<ob::Config> config = std::make_shared<ob::Config>();
-
-    int colorCount = 0;
-    int depthCount = 0;
-    try {
-        // 获取彩色相机的所有流配置，包括流的分辨率，帧率，以及帧的格式
-        auto                                    colorProfiles = pipeline.getStreamProfileList(OB_SENSOR_COLOR);
-        std::shared_ptr<ob::VideoStreamProfile> colorProfile  = nullptr;
-        if(colorProfiles) {
-            colorProfile = std::const_pointer_cast<ob::StreamProfile>(colorProfiles->getProfile(0))->as<ob::VideoStreamProfile>();
-        }
-        config->enableStream(colorProfile);
-    }
-    catch(ob::Error &e) {
-        // 没有Color Sensor
-        colorCount = -1;
-        std::cerr << "Current device is not support color sensor!" << std::endl;
-    }
-
-    // 获取深度相机的所有流配置，包括流的分辨率，帧率，以及帧的格式
-    auto                                    depthProfiles = pipeline.getStreamProfileList(OB_SENSOR_DEPTH);
-    std::shared_ptr<ob::VideoStreamProfile> depthProfile  = nullptr;
-    if(depthProfiles) {
-        depthProfile = std::const_pointer_cast<ob::StreamProfile>(depthProfiles->getProfile(0))->as<ob::VideoStreamProfile>();
-    }
-    config->enableStream(depthProfile);
-
-    // 创建格式转换Filter
-    ob::FormatConvertFilter formatConverFilter;
-
-    // 启动
-    pipeline.start(config);
-
-    int frameCount = 0;
-    while(true) {
-        // 等待一帧数据，超时时间为100ms
-        auto frameset = pipeline.waitForFrames(100);
-        if(frameset == nullptr) {
-            std::cout << "The frameset is null!" << std::endl;
-            continue;
-        }
-
-        // 过滤前面5帧数据，待数据稳定后再进行保存
-        if(frameCount < 5) {
-            frameCount++;
-            continue;
-        }
-
-        // 获取彩色和深度帧
-        auto colorFrame = frameset->colorFrame();
-        auto depthFrame = frameset->depthFrame();
-
-        if(colorFrame != nullptr && colorCount < 5) {
-            // 保存彩色图
-            colorCount++;
-            if(colorFrame->format() == OB_FORMAT_MJPG) {
-                formatConverFilter.setFormatConvertType(FORMAT_MJPG_TO_RGB888);
-            }
-            else if(colorFrame->format() == OB_FORMAT_UYVY) {
-                formatConverFilter.setFormatConvertType(FORMAT_UYVY_TO_RGB888);
-            }
-            else if(colorFrame->format() == OB_FORMAT_YUYV) {
-                formatConverFilter.setFormatConvertType(FORMAT_YUYV_TO_RGB888);
-            }
-            else {
-                std::cout << "Color format is not support!" << std::endl;
-                continue;
-            }
-            colorFrame = formatConverFilter.process(colorFrame)->as<ob::ColorFrame>();
-            formatConverFilter.setFormatConvertType(FORMAT_RGB888_TO_BGR);
-            colorFrame = formatConverFilter.process(colorFrame)->as<ob::ColorFrame>();
-            saveColor(colorFrame);
-        }
-
-        if(depthFrame != nullptr && depthCount < 5) {
-            // 保存深度图
-            depthCount++;
-            saveDepth(depthFrame);
-        }
-
-        // 当彩色图和深度图都保存5张时按ESC键退出程序
-        if(depthCount == 5 && (colorCount == 5 || colorCount == -1)) {
-            // 停止pipeline
-            pipeline.stop();
-            std::cout << "The demo is over, please press ESC to exit manually!" << std::endl;
-            int key = getch();
-
-            // 按ESC键退出
-            if(key == KEY_ESC)
-                break;
-        }
-    }
-
-    return 0;
-}
-catch(ob::Error &e) {
-    std::cerr << "function:" << e.getName() << "\nargs:" << e.getArgs() << "\nmessage:" << e.getMessage() << "\ntype:" << e.getExceptionType() << std::endl;
-    return EXIT_FAILURE;
-}
-#endif
-
 typedef struct orbbec_str {
 	ob::Pipeline pipeline;
 	std::shared_ptr<ob::Config> config;
+	//ob::Config * config;
 	bool rgb_flag;
 	bool depth_flag;
 	func_cb rgb_cb;
@@ -163,17 +58,25 @@ typedef struct orbbec_str {
 	ob::FormatConvertFilter formatConverFilter;
 } orbbec_str;
 
+/*TODO:XXX*/
+ob::Pipeline pipeline;
+ob::FormatConvertFilter formatConverFilter;
 int rgbd_init(orbbec_str ** entity, func_cb rgb, func_cb depth)
 {
 	int retval = 0;
 	*entity = (orbbec_str * )malloc(sizeof(orbbec_str));
-	//(*entity)->config = std::make_shared<ob::Config>();
+	(*entity)->config = std::make_shared<ob::Config>();
+	//(*entity)->config = (ob::Config *)malloc(sizeof(ob::Config));
+
 	if ((rgb == NULL) && ((depth == NULL))) {
 		std::cerr << "rgb or depth must have callback" << std::endl;
 		retval = -1;
 	}
 	(*entity)->rgb_cb = rgb;
-	(*entity)->depth_cb = rgb;
+	(*entity)->depth_cb = depth;
+
+	memcpy(&((*entity)->pipeline), &pipeline, sizeof(ob::Pipeline));
+	memcpy(&((*entity)->formatConverFilter), &formatConverFilter, sizeof(ob::FormatConvertFilter));
 	return retval;
 }
 
@@ -184,7 +87,8 @@ int rgbd_func_set(orbbec_str * entity, int which_func, int is_enable)
 	if (entity != NULL) {
 		if (OB_SENSOR_COLOR == which_func) {
 			try {
-				auto colorProfiles = entity->pipeline.getStreamProfileList(OB_SENSOR_COLOR);
+				os_printf("%p\n", &(entity->pipeline));
+				auto colorProfiles = (entity->pipeline).getStreamProfileList(OB_SENSOR_COLOR);
 				std::shared_ptr<ob::VideoStreamProfile> colorProfile  = nullptr;
 				if (colorProfiles) {
 					colorProfile = std::const_pointer_cast<ob::StreamProfile>(colorProfiles->getProfile(0))->as<ob::VideoStreamProfile>();
@@ -263,6 +167,9 @@ void * rgbd_runtime(void * argv)
     	int frameCount = 0;
 	orbbec_str * entity = (orbbec_str *)argv;
 	os_printf("enter runtime\n");
+
+	entity->pipeline.start(entity->config);
+
 	while (1) {
 		auto frameset = entity->pipeline.waitForFrames(100);
         	if(frameset == nullptr) {
@@ -345,6 +252,7 @@ int main()
 {
 	orbbec_str * entity;
 	rgbd_init(&entity, rgb_cb, depth_cb);
+	os_printf("%p\n", &(entity->pipeline));
 	rgb_start(entity);
 	depth_start(entity);
 	orbber_run(entity);
